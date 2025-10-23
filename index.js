40e994736b1b17869bcf9d16d124224b7899bf04
Jacksparrow7-coder <sugishiva0@gmail.com>
2025-10-23 11:29:54 +0530

    added js

---
+++ index.js
@@ -0,0 +1,12 @@
+let sidenav = document.getElementById("sidenav");
+let crossBtn = document.getElementById("closebtn");
+let menuBtn= document.getElementById("menuicon");
+
+
+menuBtn.addEventListener("click", function() {
+    sidenav.style.right = "0%";
+});
+
+crossBtn.addEventListener("click", function() {
+    sidenav.style.right = "-50%";
+});
