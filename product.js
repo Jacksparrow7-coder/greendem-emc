a0ca6e7ba86f598cac1384eea5713f3cd15a42dc
Jacksparrow7-coder <sugishiva0@gmail.com>
2025-10-24 14:43:48 +0530

    search functionality using javascript

--- product.js
+++ product.js
@@ -9,4 +9,26 @@ menuBtn.addEventListener("click", function() {
 
 crossBtn.addEventListener("click", function() {
     sidenav.style.right = "-60%";
-});
+})
+
+//searchjs code
+
+let container=document.getElementById("product-container")
+let search=document.getElementById("search")
+let productlist=container.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    let enteredvalue = event.target.value.toUpperCase()
+
+    for(count=0 ; count<productlist.length; count=count+1)
+        {
+        let productname = productlist[count].querySelector("h1").textContent
+
+        if(productname.toUpperCase().indexOf(enteredvalue)<0){
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+}
+})
